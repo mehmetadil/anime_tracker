b5ef64cf10a31a9e7c2eecd049bc9dd54264c6a1
mehmetadil <m.adil.istikbal@gmail.com>
2020-05-16 18:50:52 +0300

    Renamed Anime class to AnimeApi class. renamed search method to index. added pagination parameter to index

--- app/lib/kitsu/anime_api.rb
+++ app/lib/kitsu/anime_api.rb
@@ -2,14 +2,17 @@
 
 module Kitsu
   # Kitsu Anime Client
-  class Anime
-    def self.search(params = nil)
+  class AnimeApi
+    def self.index(params = nil)
       field_names = %w[canonicalTitle titles synopsis slug startDate endDate nextRelease status subtype
                        ageRating ageRatingGuide episodeCount episodeLength totalLength youtubeVideoId nsfw
                        coverImage posterImage]
       field_params = "?fields[anime]=#{field_names.join(',')}"
-      path = 'anime/' + field_params
-      response = Kitsu::Adaptor.get(path)
+      pagination = '&page[limit]=5&page[offset]=0'
+      path = 'anime/' + field_params + pagination
+      response = JSON.parse(Kitsu::Adaptor.get(path))
+      animes = response['data'].map { |anime| Kitsu::Serializer.new.anime(anime) }
+      animes
     end
   end
 end
