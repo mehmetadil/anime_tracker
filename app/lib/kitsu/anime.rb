f527b9d4cf9dc80004477a312f6348ec63f67819
mehmetadil <m.adil.istikbal@gmail.com>
2020-05-16 17:34:48 +0300

    Kitsu api requested fields defined

--- app/lib/kitsu/anime.rb
+++ app/lib/kitsu/anime.rb
@@ -4,10 +4,12 @@ module Kitsu
   # Kitsu Anime Client
   class Anime
     def self.search(params = nil)
-      field_names = ['synopsis', 'titles']
+      field_names = %w[canonicalTitle titles synopsis slug startDate endDate nextRelease status subtype
+                       ageRating ageRatingGuide episodeCount episodeLength totalLength youtubeVideoId nsfw
+                       coverImage posterImage]
       field_params = "?fields[anime]=#{field_names.join(',')}"
       path = 'anime/' + field_params
-      Kitsu::Adaptor.get(path)
+      response = Kitsu::Adaptor.get(path)
     end
   end
 end
